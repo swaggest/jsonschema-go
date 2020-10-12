0a5c6aefeebc298089af7d3a7726a8d54cc5584a
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2020-10-12 09:09:07 +0200

    Add IsTrivial to indicate schema that can be validated by decoder (#10)

--- helper_test.go
+++ helper_test.go
@@ -1,6 +1,7 @@
 package jsonschema_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,4 +36,59 @@ func TestSchemaOrBool_JSONSchemaBytes(t *testing.T) {
 	m, err = sbt.ToSimpleMap()
 	require.NoError(t, err)
 	assert.Equal(t, map[string]interface{}{}, m)
+}
+
+func TestSchema_IsTrivial(t *testing.T) {
+	for _, s := range []struct {
+		isTrivial bool
+		name      string
+		schema    string
+	}{
+		{true, "true schema", "true"},
+		{false, "false schema", "false"},
+		{true, "empty schema", "{}"},
+		{true, "type object", `{"type":"object", "additionalProperties":{"type":"integer"}}`},
+		{
+			false, "type object with non-trivial members",
+			`{"type":"object", "additionalProperties":{"type":"integer","minimum":3}}`,
+		},
+		{
+			true, "type object with properties",
+			`{"type":"object", "properties":{"foo":{"type":"integer"}}}`,
+		},
+		{
+			false, "type object with non-trivial members",
+			`{"type":"object", "properties":{"foo":{"type":"integer","minimum":3}}}`,
+		},
+		{false, "type fixed array", `{"type":"array", "items":[{"type":"string"}]}`},
+		{true, "type array", `{"type":"array", "items":{"type":"string"}}`},
+		{
+			false, "type array with non-trivial members",
+			`{"type":"array", "items":{"type":"string", "format":"email"}}`,
+		},
+		{true, "type array additionalItems", `{"type":"array", "additionalItems":{"type":"string"}}`},
+		{
+			false, "type array additionalItems with non-trivial members",
+			`{"type":"array", "additionalItems":{"type":"string", "format":"email"}}`,
+		},
+		{true, "scalar type", `{"type":"integer"}`},
+		{true, "scalar nullable type", `{"type":["integer", "null"]}`},
+		{false, "scalar types", `{"type":["integer", "string"]}`},
+		{false, "with format", `{"format":"email"}`},
+		{false, "with not", `{"not":true}`},
+		{false, "with allOf", `{"allOf":[true]}`},
+		{false, "with enum", `{"enum":[1,2,3]}`},
+		{false, "with minItems", `{"minItems":5}`},
+		{false, "with minProperties", `{"minProperties":5}`},
+		{false, "with $ref", `{"$ref":"#/definitions/foo","definitions":{"foo":true}}`},
+	} {
+		s := s
+
+		t.Run(s.name, func(t *testing.T) {
+			var schema jsonschema.SchemaOrBool
+
+			assert.NoError(t, json.Unmarshal([]byte(s.schema), &schema))
+			assert.Equal(t, s.isTrivial, schema.IsTrivial())
+		})
+	}
 }
