5525989fbcdf1fe9624b2db835ff8bcf78bc5654
Viacheslav Poturaev <vearutop@gmail.com>
2025-05-12 18:13:50 +0200

    Revert "Fix enums placement (#124)" (#126)
    This reverts commit 5662f8c7f3514a7aec47e667c536b7cc6198e578.

--- reflect.go
+++ reflect.go
@@ -1379,33 +1379,15 @@ func reflectEnum(schema *Schema, fieldTag reflect.StructTag, fieldVal interface{
 	enum.loadFromField(fieldTag, fieldVal)
 
 	if len(enum.items) > 0 {
-		target := searchDeepestSchema(schema)
-		target.Enum = enum.items
-
+		schema.Enum = enum.items
 		if len(enum.names) > 0 {
-			if target.ExtraProperties == nil {
-				target.ExtraProperties = make(map[string]interface{}, 1)
+			if schema.ExtraProperties == nil {
+				schema.ExtraProperties = make(map[string]interface{}, 1)
 			}
 
 			schema.ExtraProperties[XEnumNames] = enum.names
 		}
 	}
-}
-
-func searchDeepestSchema(in *Schema) *Schema {
-	if in.Items == nil {
-		return in
-	}
-
-	if in.Items.SchemaOrBool == nil {
-		return in
-	}
-
-	if in.Items.SchemaOrBool.TypeObject == nil {
-		return in
-	}
-
-	return searchDeepestSchema(in.Items.SchemaOrBool.TypeObject)
 }
 
 // enum can be use for sending enum data that need validate.
